60f06f8933c7284f4f04a11c4ebfe5a89b73aa49
BhagyashreeGhodke <drbpghodke@gmail.com>
2024-02-22 05:07:09 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+
+  const [username, setUsername] = useState("")
+
+  useEffect( () => {
+    axios.get('/api/v1/login')
+    .then( (response) => {
+      setUsername(response.data)
+      console.log("response: ",response);
+      console.log("response.data: ",response.data);
+    })
+    .catch( (error) => {
+      console.log("check axios in App.jsx :: error :: ",error);
+    })
+  }, [])
   
   return (
     <>
       <h1>FlakeTube</h1>
+      <p>Username: {username}</p>
+
     </>
   )
 }
